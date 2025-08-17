9d5333ecd6c2056c36fc3fd3efdf73528c4238ec
raisanjeeb <sanjeebraix@gmail.com>
2025-08-17 17:20:00 +0545

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,14 @@
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+num1 = int(num1)
+num2 = int(num2)
+
+print("Sum:", num1 + num2)
+print("Difference:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Modulus:", num1 % num2)
+print("Integer Division:", num1 // num2)
+print("Power:", num1 ** num2)
